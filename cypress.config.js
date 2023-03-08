6b8b8668140e1468f6f7fb719cdf4f237f306983
Olena CHyrkina <chirkina1604gmail.com>
2023-03-08 16:58:18 +0100

    add config changes

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  viewportHeight: 1080,
+  viewportWidth: 1920,
   e2e: {
+    baseUrl:'https://juice-shop-sanitarskyi.herokuapp.com/#/search',
     setupNodeEvents(on, config) {
+    
       // implement node event listeners here
     },
   },
